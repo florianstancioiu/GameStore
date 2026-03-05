f324a82d4e122c3b2332bffb01d43f53117c2c04
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2026-03-05 15:25:13 +0200

    Added validation using DataAnnotations

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,7 +1,9 @@
-using GameStore.Api.Dtos;
 using GameStore.Api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddValidation();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "The GameStore.Api is up and running");
