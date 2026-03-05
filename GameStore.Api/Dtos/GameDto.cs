9b53467d1499840078b07af4ad5da814fb973f6b
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2026-03-05 13:53:54 +0200

    Implemented the basic /games route

---
+++ GameStore.Api/Dtos/GameDto.cs
@@ -0,0 +1,12 @@
+namespace GameStore.Api.Dtos;
+
+// A DTO is a contract between the client and server
+// it represents how the data will be transferred and used
+
+public record GameDto(
+    int Id,
+    string Name,
+    string Genre,
+    decimal Price,
+    DateOnly ReleaseDate
+);
--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,6 +1,15 @@
+using GameStore.Api.Dtos;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+List<GameDto> games = [
+    new (1, "Street fighter 2", "Fighting", 19.99M, new DateOnly(1992, 7, 15)),
+    new (2, "Final fantasy VII rebirth", "RPG", 69.99M, new DateOnly(2024, 2, 29)),
+    new (3, "Astro bot", "Platformer", 59.99M, new DateOnly(2024, 9, 6)),
+];
+
+// GET /games
+app.MapGet("/games", () => games);
 
 app.Run();
